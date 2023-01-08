597ec9861a3da0ac9e631b9a240420b62e89253b
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-08 15:02:05 +0100

    add small test app

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,22 +1,24 @@
 import { appRouter, createContext } from "@acme/api";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
+import { NextApiRequest, NextApiResponse } from "next";
 
 // If you need to enable cors, you can do so like this:
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   // Enable cors
-//   await cors(req, res);
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  // Enable cors
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    return res.end();
+  }
 
-//   // Let the tRPC handler do its magic
-//   return createNextApiHandler({
-//     router: appRouter,
-//     createContext,
-//   })(req, res);
-// };
+  // Let the tRPC handler do its magic
+  return createNextApiHandler({
+    router: appRouter,
+    createContext,
+  })(req, res);
+};
 
-// export default handler;
+export default handler;
